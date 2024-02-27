6381eadcba7b9d5c7fcdf5c421288b6dda427d00
SahajArora <sahajarora1906@gmail.com>
2024-02-27 14:17:46 +0530

    Updated index, added book model, and modified package files

--- express with mongodb/index.js
+++ express with mongodb/index.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+const Book = require("./models/book.js");
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 async function main() {
@@ -12,4 +18,27 @@ async function main() {
 }
 
 main();
+
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.engine("ejs", ejsMate);
+app.set("views", path.join(__dirname, "views"));
+app.use(methodOverride("_method"));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>
+      Welcome to RESTAPI tutorial <br /> Express with MongoDB
+    </h1>`
+  );
+});
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+app.listen(3000, function () {
+  console.log("server is active on http://localhost:3000");
+});
